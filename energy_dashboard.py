4d6866722530d6e40efb8d72603bcc77f642285c
Yousra Jafrin <94543411+YousraJaf@users.noreply.github.com>
2022-04-08 17:09:12 +0200

    correction1

--- energy_dashboard.py
+++ energy_dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 import holoviews as hv
 from holoviews import opts
